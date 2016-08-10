0d2191670d444b9fe648c07a677cb001a726e270
ivanh96 <ivanh96@hotmail.com>
2016-08-10 09:31:10 -0600

    Update DoubleLinkedList[E].java

--- DoubleLinkedList[E].java
+++ DoubleLinkedList[E].java
@@ -4,23 +4,23 @@ import java.util.*;
 /**
  * 
  */
-public class DoubleLinkedList[E] extends AbstractList {
+public class DoubleLinkedList<E> extends AbstractList {
 
     /**
      * 
      */
-    public DoubleLinkedList[E]() {
+    public DoubleLinkedList<E>() {
     }
 
     /**
      * 
      */
-    private DoubleNode[E] head;
+    private DoubleNode<E> head;
 
     /**
      * 
      */
-    private DoubleNode[E] tail;
+    private DoubleNode<E> tail;
 
     /**
      * 
